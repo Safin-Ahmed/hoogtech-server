ff691516410d1266f329b143b500bb5e00af2a4a
Safin-Ahmed <safin.ahmed2000@gmail.com>
2021-11-13 21:23:58 +0600

    Fixed problems

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-var serviceAccount = require(`${process.env.FIREBASE_SERVICE_ACCOUNT}`);
+var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
