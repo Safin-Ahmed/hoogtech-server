ce1b525bb5d067c0b227c30d91548b618e12b1cd
Safin Ahmed <safin.ahmed2000@gmail.com>
2022-11-24 21:09:07 +0600

    updated appconfig

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ app.use(cors());
 app.use(express.json())
 
 
+const appConfig = import('./hoogtech-firebase-adminsdk.json');
+
 admin.initializeApp({
-    credential: admin.credential.cert(process.env.FIREBASE_SERVICE_ACCOUNT)
+    credential: admin.credential.cert(appConfig)
 });
 
 async function verifyToken(req, res, next) {
